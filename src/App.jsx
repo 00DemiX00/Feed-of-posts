eeebd098b6ea291ff301e772667e9d7d768d4342
00DemiX00 <alescha.yarkin@yandex.ru>
2024-07-24 19:55:21 +0300

    Начало

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Styles/App.css';
+import Counter from './Components/Counter';
+import ClassCounter from './Components/ClassCounter';
+import PostItem from './Components/PostItem';
+import PostList from './Components/PostList';
+import MyButton from './Components/UI/Button/MyButton';
+
+
+
+function App() {
+  
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Название поста 1', body: 'Описание поста 1'},
+    {id: 2, title: 'Название поста 2', body: 'Описание поста 2'},
+    {id: 3, title: 'Название поста 3', body: 'Описание поста 3'},
+  ])
+  
+  return (
+  <div className='App'>
+    <form>
+      <input type='text' placeholder='Название поста'></input>
+      <input type='text' placeholder='Название поста'></input>
+      <button>Создать пост</button>
+    </form>
+     <PostList posts={posts} title="Список постов 1"></PostList>
+
+  </div>
+  );
+}
+
+export default App
---
+++ src/Components/UI/Button/MyButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const MyButton = (props) => {
+    return (
+        <button className={classes}>
+
+        </button>
+    );
+};
+
+
+
+
+export default MyButton;

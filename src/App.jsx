2a0caf5b81b3472e92c692200a46d0cccb862008
00DemiX00 <alescha.yarkin@yandex.ru>
2024-07-30 19:50:51 +0300

    Предотвратил обновление страницы из-за типа submit формы (preventDefault), добавил функцию создания нового поста

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,29 @@ function App() {
     {id: 3, title: 'Название поста 3', body: 'Описание поста 3'},
   ])
   
+const [title, setTitle] = useState()
+const [body, setBody] = useState()
+
+
+
+const addNewPost = (e) => {
+  e.preventDefault()
+  const newPost = {
+    id: Date.now(),
+    title,
+    body
+  }
+ setPosts ([...posts, newPost]);
+ setTitle ('');
+ setBody ('');
+}
+
   return (
   <div className='App'>
     <form>
-      <MyInput type='text' placeholder='Название поста'></MyInput>
-      <MyInput type='text' placeholder='Название поста'></MyInput>
-      <MyButton>Создать пост</MyButton>
+      <MyInput value={title} onChange={e => setTitle(e.target.value)} type='text' placeholder='Название поста'></MyInput>
+      <MyInput value={body} onChange={e => setBody(e.target.value)} type='text' placeholder='Название поста'></MyInput>
+      <MyButton onClick={addNewPost}>Создать пост</MyButton>
     </form>
      <PostList posts={posts} title="Список постов 1"></PostList>
 

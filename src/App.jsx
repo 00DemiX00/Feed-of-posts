5b57b28a6de85e17627faab3a4a25f9a08609a76
00DemiX00 <alescha.yarkin@yandex.ru>
2024-08-01 17:35:12 +0300

    Реализовал функционал удаления постов из ленты

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,14 @@ function App() {
     setPosts ([...posts, newPost])
   };
 
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
   <div className='App'>
       <PostForm create={createPost}></PostForm>
-      <PostList posts={posts} title="Список постов 1"></PostList>
+      <PostList remove={removePost} posts={posts} title="Список постов 1"></PostList>
 
   </div>
   )
--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -17,7 +17,7 @@ const PostForm = ({create}) => {
     return (
     <form>
       <MyInput value={post.title} onChange={e => setPost({...post, title: e.target.value})} type='text' placeholder='Название поста'></MyInput>
-      <MyInput value={post.body} onChange={e => setPost({...post, body: e.target.value})} type='text' placeholder='Название поста'></MyInput>
+      <MyInput value={post.body} onChange={e => setPost({...post, body: e.target.value})} type='text' placeholder='Описание поста'></MyInput>
       <MyButton onClick={addNewPost}>Создать пост</MyButton>
     </form>
     )

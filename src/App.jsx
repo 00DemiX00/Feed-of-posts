49c1c0b4e7c60d21405f7125b8ba1e2e8f3146dc
00DemiX00 <alescha.yarkin@yandex.ru>
2024-08-01 16:13:36 +0300

    Создал компонент "PostForm", куда поместил form 'MyInput, MyInput, MyButton'. Из родительсткого компонента App в дочерний PostForm передал callback для вызова формы создания постов

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,28 @@ import PostItem from './Components/PostItem';
 import PostList from './Components/PostList';
 import MyButton from './Components/UI/Button/MyButton';
 import MyInput from './Components/UI/Input/MyInput';
-
+import PostForm from './Components/PostForm';
 
 
 function App() {
-  
+
   const [posts, setPosts] = useState([
     {id: 1, title: 'Название поста 1', body: 'Описание поста 1'},
     {id: 2, title: 'Название поста 2', body: 'Описание поста 2'},
     {id: 3, title: 'Название поста 3', body: 'Описание поста 3'},
   ])
   
-const [post, setPost] = useState({title: '', body: ''})
-
-
-
-const addNewPost = (e) => {
-  e.preventDefault()
-  setPosts ([...posts, {...post, id: Date.now()}]);
-  setPost ({title: '', body: ''})
-  }
+  const createPost = (newPost) => {
+    setPosts ([...posts, newPost])
+  };
 
   return (
   <div className='App'>
-    <form>
-      <MyInput value={post.title} onChange={e => setPost({...post, title: e.target.value})} type='text' placeholder='Название поста'></MyInput>
-      <MyInput value={post.body} onChange={e => setPost({...post, body: e.target.value})} type='text' placeholder='Название поста'></MyInput>
-      <MyButton onClick={addNewPost}>Создать пост</MyButton>
-    </form>
-     <PostList posts={posts} title="Список постов 1"></PostList>
+      <PostForm create={createPost}></PostForm>
+      <PostList posts={posts} title="Список постов 1"></PostList>
 
   </div>
-  );
+  )
 }
 
 export default App

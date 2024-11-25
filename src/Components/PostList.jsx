96ee47ba16f9b1a020198b5ad851f3ecb2257a4d
00DemiX00 <alescha.yarkin@yandex.ru>
2024-11-25 20:07:55 +0300

    Прописал анимации с помощью Transition

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PostItem from "./PostItem";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 
 const PostList = ({posts, title, remove}) => {
 
@@ -14,9 +16,16 @@ if (!posts.length) {
     return (
     <div>
         <h1>{title}</h1>
+        <TransitionGroup>
         {posts.map((post, index) =>
-            <PostItem remove={remove} number={index + 1} post={post} key={post.id}></PostItem>
-    )}
+        <CSSTransition 
+            key={post.id} 
+            timeout={500} 
+        classNames="post">
+            <PostItem remove={remove} number={index + 1} post={post}></PostItem>
+        </CSSTransition>        
+        )}
+        </TransitionGroup>
     </div>
     );
 };
